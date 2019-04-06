25080cbbfb2fb4d2e1d50a0b208a4432bfcc9230
Eduardo Pedrosa <dudupedrosao@gmail.com>
2019-04-06 14:36:20 -0300

    verificacao palavras reservadas

--- src/AnalisadorLexico.java
+++ src/AnalisadorLexico.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class AnalisadorLexico {
@@ -12,5 +14,213 @@ public class AnalisadorLexico {
 		palavrasReservadas.put("while",Palavras.WHILE);
 		palavrasReservadas.put("void",Palavras.VOID);
 		palavrasReservadas.put("return",Palavras.RETURN);
+	}
+	
+	public void Analisar(BufferedReader arquivo) throws IOException {
+		char caracter = ' ';
+		while(arquivo.ready()) { //ENTRANDO EM LOOP INFINITO PORQUE O READY TA SEMPRE ENTRANDO
+			String palavra = "";
+			boolean encontrouPalavra = false;
+			if(caracter == ' ' || caracter == '\n') {
+				caracter = (char) arquivo.read();
+			}
+			boolean primeiroCaracter = true;
+			if(caracter == 'i') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'f') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+						palavra += caracter;
+					} else {
+						encontrouPalavra = true;
+						//criar token
+					}
+				}
+				if(caracter == 'n') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 't') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+							palavra += caracter;
+						} else {
+							encontrouPalavra = true;
+							//criar token
+						}
+					}
+				}
+			} else if(caracter == 'f'){
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'l') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 'o') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'a') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(caracter == 't') {
+								palavra += caracter;
+								caracter = (char) arquivo.read();
+								if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+									palavra += caracter;
+								} else {
+									encontrouPalavra = true;
+									//criar token
+								}
+							}
+						}
+					}
+				}
+			} else if(caracter == 's') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 't') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 'r') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'u') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(caracter == 'c') {
+								palavra += caracter;
+								caracter = (char) arquivo.read();
+								if(caracter == 't') {
+									palavra += caracter;
+									caracter = (char) arquivo.read();
+									if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+										palavra += caracter;
+									} else {
+										encontrouPalavra = true;
+										//criar token
+									}
+								}
+							}
+						}
+					}
+				}
+			} else if(caracter == 'e') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'l') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 's') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'e') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+								palavra += caracter;
+							} else {
+								encontrouPalavra = true;
+								//criar token
+							}
+						}
+					}
+				}
+			} else if(caracter == 'w') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'h') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 'i') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'l') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(caracter == 'e') {
+								palavra += caracter;
+								caracter = (char) arquivo.read();
+								if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+									palavra += caracter;
+								} else {
+									encontrouPalavra = true;
+									//criar token
+								}
+							}
+						}
+					}
+				}
+			} else if(caracter == 'v') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'o') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 'i') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'd') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+								palavra += caracter;
+							} else {
+								encontrouPalavra = true;
+								//criar token
+							}
+						}
+					}
+				}
+			} else if(caracter == 'r') {
+				palavra += caracter;
+				primeiroCaracter = false;
+				caracter = (char) arquivo.read();
+				if(caracter == 'e') {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(caracter == 't') {
+						palavra += caracter;
+						caracter = (char) arquivo.read();
+						if(caracter == 'u') {
+							palavra += caracter;
+							caracter = (char) arquivo.read();
+							if(caracter == 'r') {
+								palavra += caracter;
+								caracter = (char) arquivo.read();
+								if(caracter == 'n') {
+									palavra += caracter;
+									caracter = (char) arquivo.read();
+									if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+										palavra += caracter;
+									} else {
+										encontrouPalavra = true;
+										//criar token
+									}
+								}
+							}
+						}
+					}
+				}
+			} 
+			if(!encontrouPalavra) {
+				while((Character.isDigit(caracter) && !primeiroCaracter) || Character.isLetter(caracter)) {
+					palavra += caracter;
+					caracter = (char) arquivo.read();
+					if(Character.isDigit(caracter) || Character.isLetter(caracter)) {
+						palavra += caracter;
+					}
+				}
+				System.out.println(palavra);
+				//se não, criar token de identificador
+			}
+		}
 	}
 }
