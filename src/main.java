b5ab12a321b7d9d0010e4c3cdf2fd51e1addd585
Sid <pedro.fachini.99@gmail.com>
2019-04-11 11:08:43 -0300

    main e um teste adicionados

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class main {
+    public static void main(String[] args) {
+        AnalisadorLexico analisadorLex = new AnalisadorLexico();
+        try {
+            Scanner entrada = new Scanner(System.in);
+            String nomeArq = entrada.nextLine();
+            BufferedReader leitor = new BufferedReader(new FileReader(nomeArq));
+            analisadorLex.Analisar(leitor);
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
